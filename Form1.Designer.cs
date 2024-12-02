75c59e2843d2b8e13495b4a67146b0d1fd53411f
costa <costa@DESKTOP-EOOHF5T>
2024-12-02 14:18:59 -0400

    subida

---
+++ Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace Inventario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(861, 412);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtCodigoItem;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button btnInsertar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using ClosedXML.Excel;
+using FirebirdSql.Data.FirebirdClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Cadena de conexión a Firebird
+        private string connectionString = @"User=SYSDBA;Password=masterkey;Database=C:\NemeSys\Database\NEMESYSDB.IB;DataSource=localhost;Port=3050;Dialect=3;Charset=UTF8;";
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Conectar a la base de datos
+                using (FbConnection connection = new FbConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Consulta SQL para insertar datos
+                    string query = @"
+                        INSERT INTO INVENTARIO (CODIGO_ITEM, DESC1, PRECIO, CANTIDAD)
+                        VALUES (@CodigoItem, @Desc1, @Precio, @Cantidad)";
+
+                    using (FbCommand command = new FbCommand(query, connection))
+                    {
+                        // Agregar parámetros
+                        command.Parameters.AddWithValue("@CodigoItem", txtCodigoItem.Text);
+                        command.Parameters.AddWithValue("@Desc1", txtDescripcion.Text);
+                        command.Parameters.AddWithValue("@Precio", Convert.ToDecimal(txtPrecio.Text));
+                        command.Parameters.AddWithValue("@Cantidad", Convert.ToInt32(txtCantidad.Text));
+
+                        // Ejecutar consulta
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Datos insertados correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se pudo insertar los datos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+    }
